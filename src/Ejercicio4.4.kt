02b773e84c7fa3a210d7c3f758e4f1f67d5ee9d8
HRJuan580 <jherrel580@g.educaand.es>
2025-01-21 16:39:50 +0100

    Nuevo Ejercicio4.4.kt creado

--- src/Ejercicio4.4.kt
+++ src/Ejercicio4.4.kt
@@ -1,3 +1,7 @@
-fun main(){
-    println("Hola, kotlin")
-}
+class Coche(val color: String,
+            val marca: String,
+            val Ncaballos: Double,
+            val Npuertas: Double,
+            val matricula: Double){
+
+} 
